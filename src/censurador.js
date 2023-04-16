7669f538a5368cffacfbe89bf301f5314cc2af86
Felipe Gabriel <67085132+fsilva19@users.noreply.github.com>
2023-04-15 22:58:58 -0300

    Update censurador.js

--- src/censurador.js
+++ src/censurador.js
@@ -1,5 +1,4 @@
 var Filter = require('bad-words');
-//import { Filter } from "../node_modules/bad-words/lib";
 var filter = new Filter();
 
 const formulario = document.getElementById('formulario');
@@ -8,6 +7,7 @@ const censurado = document.getElementById('mudar');
 formulario.addEventListener('submit', function(e){
     e.preventDefault();
     var mudar = formulario.querySelector('#texto').value;
+    
     filter.addWords('putaria','porno','pornô','rola','pica','boquete','buceta','priquito','prikito','porra','caralho','carai','karai','pornografia');
     if(filter.isProfane(mudar)){
         censurado.innerText= 'ATENÇÃO!\nO texto contém palavras obscenas';
